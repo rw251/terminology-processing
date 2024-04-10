03e187fa1dfe51db25adc55f472fe109715d4050
Richard Williams <1234richardwilliams@gmail.com>
2024-04-10 11:51:45 +0100

    Added nhs pcd refset grabber

---
+++ nhs-pcd-refset/index.js
@@ -0,0 +1,415 @@
+/*
+ * 1. Gets the latest zip file from the TRUD website (unless already downloaded)
+ *  - You need a TRUD account
+ *  - Put your login detatils in the .env file
+ *  - Make sure you are subscribed to "SNOMED CT UK Drug Extension, RF2: Full, Snapshot & Delta"
+ * 2.
+ *
+ */
+
+import {
+  existsSync,
+  readFileSync,
+  readdirSync,
+  createWriteStream,
+  createReadStream,
+  writeFileSync,
+} from 'fs';
+import path from 'path';
+import unzip from 'unzip-stream';
+import { brotliCompress } from '../lib/brotli-compress.js';
+import { uploadToR2 } from '../lib/cloudflare.js';
+import { getLatestPcdRefsetUrl, downloadFile } from '../lib/trud.js';
+import { ensureDir, getSnomedDefinitions, getDirName } from '../lib/utils.js';
+
+const __dirname = getDirName(import.meta.url);
+
+const FILES_DIR = path.join(__dirname, 'files');
+const ZIP_DIR = ensureDir(path.join(FILES_DIR, 'zip/'), true);
+const RAW_DIR = ensureDir(path.join(FILES_DIR, 'raw'), true);
+const PROCESSED_DIR = ensureDir(path.join(FILES_DIR, 'processed'), true);
+
+const existingFiles = readdirSync(ZIP_DIR);
+
+let SNOMED_DEFINITIONS;
+
+async function downloadIfNotExists({ url }) {
+  const zipFileName = url.split('/').reverse()[0].split('?')[0];
+  console.log(`> Target zip file on TRUD is ${zipFileName}`);
+
+  if (existingFiles.indexOf(zipFileName) > -1) {
+    console.log(`> The zip file already exists so no need to download again.`);
+    return { zipFileName };
+  }
+
+  console.log(`> That zip is not stored locally. Downloading...`);
+  const outputFile = path.join(ZIP_DIR, zipFileName);
+
+  await downloadFile(url, outputFile);
+
+  return { zipFileName };
+}
+
+async function extractZip({ zipFileName }) {
+  const dirName = zipFileName.replace('.zip', '');
+  const file = path.join(ZIP_DIR, zipFileName);
+  const outDir = path.join(RAW_DIR, dirName);
+  if (existsSync(outDir)) {
+    console.log(
+      `> The directory ${outDir} already exists, so I'm not unzipping.`
+    );
+    return { dirName };
+  }
+  console.log(`> The directory ${outDir} does not yet exist. Creating...`);
+  ensureDir(outDir, true);
+  console.log(`> Extracting files from the zip...`);
+  let toUnzip = 0;
+  let unzipped = 0;
+  let isRead = false;
+  await new Promise((resolve) => {
+    createReadStream(file)
+      .pipe(unzip.Parse())
+      .on('entry', function (entry) {
+        /*
+              file.path.toLowerCase().indexOf('full') > -1
+      file.path.toLowerCase().indexOf('readme') > -1
+      file.path.toLowerCase().indexOf('information') > -1
+        */
+        if (
+          entry.path.toLowerCase().match(/full.+content.+refset_simple/) ||
+          entry.path.toLowerCase().match(/full.+sct2_description/)
+        ) {
+          console.log(`> Extracting ${entry.path}...`);
+          toUnzip++;
+          const outputFilePath = path.join(outDir, entry.path);
+          const outStream = createWriteStream(ensureDir(outputFilePath));
+          outStream.on('finish', () => {
+            console.log(`> Extracted ${entry.path}.`);
+            unzipped++;
+            if (isRead && toUnzip === unzipped) {
+              return resolve();
+            }
+          });
+          entry.pipe(outStream);
+        } else {
+          entry.autodrain();
+        }
+      })
+      .on('end', () => {
+        console.log(`> Finished reading zip file.`);
+        isRead = true;
+        if (toUnzip === unzipped) {
+          return resolve();
+        }
+      });
+  });
+  console.log(`> ${unzipped} files extracted.`);
+  return { dirName };
+}
+
+function getFileNames(dir) {
+  const rawFilesDir = path.join(RAW_DIR, dir);
+  const version = path.basename(dir);
+  const processedFilesDir = path.join(PROCESSED_DIR, dir);
+  const definitionJsonFile = path.join(processedFilesDir, 'pcd-defs.json');
+  const refSetJsonFile = path.join(processedFilesDir, 'pcd-refSets.json');
+  const definitionFileBrotli = path.join(processedFilesDir, 'pcd-defs.json.br');
+  const refSetFileBrotli = path.join(processedFilesDir, 'pcd-refSets.json.br');
+  return {
+    version,
+    rawFilesDir,
+    definitionJsonFile,
+    refSetJsonFile,
+    definitionFileBrotli,
+    refSetFileBrotli,
+    processedFilesDir,
+  };
+}
+
+async function loadDataIntoMemory({ dirName }) {
+  const { processedFilesDir, rawFilesDir, definitionJsonFile, refSetJsonFile } =
+    getFileNames(dirName);
+  if (existsSync(definitionJsonFile) && existsSync(refSetJsonFile)) {
+    console.log(`> The json files already exist so I'll move on...`);
+    return dirName;
+  }
+  ensureDir(processedFilesDir, true);
+
+  const PCD_DIR = path.join(
+    rawFilesDir,
+    readdirSync(rawFilesDir).filter((x) => x.indexOf('PrimaryCare') > -1)[0]
+  );
+  const REFSET_DIR = path.join(PCD_DIR, 'Full', 'Refset', 'Content');
+  const refsetFile = path.join(
+    REFSET_DIR,
+    readdirSync(REFSET_DIR).filter((x) => x.indexOf('Simple') > -1)[0]
+  );
+  const refSets = {};
+  const allConcepts = {};
+  readFileSync(refsetFile, 'utf8')
+    .split('\n')
+    .forEach((row) => {
+      const [
+        id,
+        effectiveTime,
+        active,
+        moduleId,
+        refsetId,
+        referencedComponentId,
+      ] = row.replace(/\r/g, '').split('\t');
+      if (id === 'id' || !referencedComponentId) return;
+      if (!refSets[refsetId]) {
+        allConcepts[refsetId] = true;
+        refSets[refsetId] = {};
+      }
+      if (!refSets[refsetId][id]) {
+        refSets[refsetId][id] = {
+          effectiveTime,
+          conceptId: referencedComponentId,
+        };
+        if (active === '1') {
+          refSets[refsetId][id].active = true;
+        }
+      } else {
+        if (refSets[refsetId][id].conceptId !== referencedComponentId) {
+          console.log(
+            `An unexpected error. I thought that if the id (${id}) was the same, then the conceptid (${referencedComponentId}) would be the same.`
+          );
+          console.log('Need to rewrite the code...');
+          process.exit();
+        }
+        if (effectiveTime > refSets[refsetId][id].effectiveTime) {
+          refSets[refsetId][id].effectiveTime = effectiveTime;
+          refSets[refsetId][id].active = active === '1';
+        }
+      }
+      allConcepts[referencedComponentId] = true;
+    });
+  console.log(
+    `> Ref set file loaded. It has ${Object.keys(refSets).length} rows.`
+  );
+
+  // Now process it a bit
+  Object.keys(refSets).forEach((refSetId) => {
+    const active = Array.from(
+      new Set(
+        Object.values(refSets[refSetId])
+          .filter((x) => x.active)
+          .map((x) => x.conceptId)
+      )
+    );
+    const inactive = Array.from(
+      new Set(
+        Object.values(refSets[refSetId])
+          .filter((x) => !x.active)
+          .map((x) => x.conceptId)
+      )
+    );
+    refSets[refSetId] = {
+      active,
+      inactive,
+    };
+  });
+
+  SNOMED_DEFINITIONS = getSnomedDefinitions();
+  const snomedDefsSize = Object.keys(SNOMED_DEFINITIONS).length;
+  const TERM_DIR = path.join(PCD_DIR, 'Full', 'Terminology');
+  const descFile = path.join(
+    TERM_DIR,
+    readdirSync(TERM_DIR).filter((x) => x.indexOf('_Description_') > -1)[0]
+  );
+  readFileSync(descFile, 'utf8')
+    .split('\n')
+    .forEach((row) => {
+      const [
+        id,
+        effectiveTime,
+        active,
+        moduleId,
+        conceptId,
+        languageCode,
+        typeId,
+        term,
+        caseSignificanceId,
+      ] = row.replace(/\r/g, '').split('\t');
+      if (id === 'id' || !conceptId) return;
+
+      if (!SNOMED_DEFINITIONS[conceptId]) SNOMED_DEFINITIONS[conceptId] = {};
+      if (!SNOMED_DEFINITIONS[conceptId][id]) {
+        SNOMED_DEFINITIONS[conceptId][id] = { t: term, e: effectiveTime };
+        if (active === '1') {
+          SNOMED_DEFINITIONS[conceptId][id].a = 1;
+        }
+        if (typeId === '900000000000003001') {
+          SNOMED_DEFINITIONS[conceptId][id].m = 1;
+        }
+      } else {
+        if (effectiveTime > SNOMED_DEFINITIONS[conceptId][id].e) {
+          SNOMED_DEFINITIONS[conceptId][id].t = term;
+          SNOMED_DEFINITIONS[conceptId][id].e = effectiveTime;
+          if (active === '1') {
+            SNOMED_DEFINITIONS[conceptId][id].a = 1;
+          } else {
+            delete SNOMED_DEFINITIONS[conceptId][id].a;
+          }
+          if (typeId === '900000000000003001') {
+            SNOMED_DEFINITIONS[conceptId][id].m = 1;
+          } else {
+            delete SNOMED_DEFINITIONS[conceptId][id].m;
+          }
+        }
+      }
+    });
+  //
+  console.log(
+    `> Description file loaded and added to main SNOMED dictionary.
+    Previously the SNOMED dictionary had ${snomedDefsSize} concepts.
+    It now has ${Object.keys(SNOMED_DEFINITIONS).length} concepts.`
+  );
+
+  const simpleDefs = {};
+
+  Object.keys(allConcepts).forEach((conceptId) => {
+    if (SNOMED_DEFINITIONS[conceptId]) {
+      // pick best definition
+
+      // if we have any that are active AND main then pick most recent
+      const activeAndMainDef = Object.values(SNOMED_DEFINITIONS[conceptId])
+        .filter((data) => data.a && data.m)
+        .sort((a, b) => {
+          if (a.e > b.e) return -1;
+          return a.e === b.e ? 0 : 1;
+        });
+
+      if (activeAndMainDef.length > 0) {
+        simpleDefs[conceptId] = activeAndMainDef[0];
+        return;
+      }
+
+      // if no mains, but some actives, pick most recent
+      const activeAndSynDef = Object.values(SNOMED_DEFINITIONS[conceptId])
+        .filter((data) => data.a && !data.m)
+        .sort((a, b) => {
+          if (a.e > b.e) return -1;
+          return a.e === b.e ? 0 : 1;
+        });
+
+      if (activeAndSynDef.length > 0) {
+        simpleDefs[conceptId] = activeAndSynDef[0];
+        return;
+      }
+
+      // if main but no actives, pick most recent
+      const inactiveAndMainDef = Object.values(SNOMED_DEFINITIONS[conceptId])
+        .filter((data) => !data.a && data.m)
+        .sort((a, b) => {
+          if (a.e > b.e) return -1;
+          return a.e === b.e ? 0 : 1;
+        });
+
+      if (inactiveAndMainDef.length > 0) {
+        simpleDefs[conceptId] = inactiveAndMainDef[0];
+        return;
+      }
+
+      // no main and no active - investigate
+      const inactiveAndMSynDef = Object.values(SNOMED_DEFINITIONS[conceptId])
+        .filter((data) => !data.a && !data.m)
+        .sort((a, b) => {
+          if (a.e > b.e) return -1;
+          return a.e === b.e ? 0 : 1;
+        });
+
+      if (inactiveAndMSynDef.length > 0) {
+        simpleDefs[conceptId] = inactiveAndMSynDef[0];
+        return;
+      }
+      console.log(`ERROR - no defintions found at all for ${conceptId}`);
+    } else {
+      //console.log(conceptId);
+      //TODO? maybe keep track of them here?
+    }
+  });
+
+  const simpleRefSets = {};
+
+  Object.keys(refSets).forEach((refSetId) => {
+    if (!simpleDefs[refSetId])
+      console.log(`No description for refset with id: ${refSetId}`);
+    else {
+      const def = simpleDefs[refSetId].t;
+      if (simpleRefSets[def])
+        console.log(`There is already an entry for: ${def}`);
+      else {
+        simpleRefSets[def] = refSets[refSetId];
+      }
+    }
+  });
+
+  // Find snomed codes without definition
+
+  const unknownCodes = Object.values(simpleRefSets)
+    .map((x) => x.active.concat(x.inactive))
+    .flat()
+    .filter((conceptId) => !simpleDefs[conceptId]);
+
+  if (unknownCodes.length > 0) {
+    console.log(
+      `> There are ${unknownCodes.length} unknown codes. This shouldn't happen.`
+    );
+    console.log(unknownCodes.join('\n'));
+    process.exit();
+  }
+
+  writeFileSync(definitionJsonFile, JSON.stringify(simpleDefs, null, 2));
+  writeFileSync(refSetJsonFile, JSON.stringify(simpleRefSets, null, 2));
+
+  return dirName;
+}
+
+function compressJson(dir) {
+  const {
+    definitionJsonFile,
+    refSetJsonFile,
+    definitionFileBrotli,
+    refSetFileBrotli,
+  } = getFileNames(dir);
+  if (existsSync(definitionFileBrotli) && existsSync(refSetFileBrotli)) {
+    console.log(`> The brotli files already exist so I'll move on...`);
+    return dir;
+  }
+
+  console.log('> Starting compression...');
+
+  brotliCompress(refSetJsonFile);
+  brotliCompress(definitionJsonFile);
+  console.log(`> All compressed.`);
+  return dir;
+}
+
+async function upload(dir) {
+  const { definitionJsonFile, refSetJsonFile, version } = getFileNames(
+    dir,
+    true
+  );
+
+  await uploadToR2(
+    definitionJsonFile,
+    path.join('NHS-PCD-REFSET', version, path.basename(definitionJsonFile))
+  );
+  await uploadToR2(
+    refSetJsonFile,
+    path.join('NHS-PCD-REFSET', version, path.basename(refSetJsonFile))
+  );
+}
+
+async function processLatestNHSPCDRefsets() {
+  await getLatestPcdRefsetUrl()
+    .then(downloadIfNotExists)
+    .then(extractZip)
+    .then(loadDataIntoMemory)
+    .then(compressJson)
+    .then(upload);
+}
+
+export { processLatestNHSPCDRefsets };
