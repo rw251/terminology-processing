2577ab13dd32ab97f510cf79f62a81bf8ba46b7d
Richard Williams <1234richardwilliams@gmail.com>
2024-04-19 16:10:51 +0100

    Initial web commit

---
+++ web/script.js
@@ -0,0 +1,269 @@
+const $results = document.getElementById('results');
+const $versionWrapper = document.getElementById('version-wrapper');
+const $overview = document.getElementById('overview');
+const $wrapper = document.querySelector('.wrapper');
+const $extraTerms = document.getElementById('extra-terms');
+const $descendantCodes = document.querySelector('#results tbody');
+const $message = document.getElementById('message');
+const $copyAll = document.getElementById('copy-all');
+const $wordInput = document.getElementById('input-word');
+const $toggleInactive = document.getElementById('show-inactive');
+const $definitionsTable = document.getElementById('defs-table');
+const $loader = document.getElementById('loader');
+const $excludedCodes = document.getElementById('excluded-codes');
+const $tab = document.querySelector('.tab');
+const $termTab = document.getElementById('term-tab');
+const $searchResult = document.querySelector(
+  '.autocomplete-search-box .search-result'
+);
+
+const workerUrl = '/worker.js?v=1.1.9';
+const worker = {
+  ctv3: new Worker(workerUrl, { name: 'ctv3' }),
+  readv2: new Worker(workerUrl, { name: 'readv2' }),
+  snomed: new Worker(workerUrl, { name: 'snomed' }),
+};
+
+function send(terminology, action, params = {}) {
+  worker[terminology].postMessage({ action, params });
+}
+
+function onWorkerMessage(e) {
+  const { msg, terminology } = e.data;
+  switch (msg) {
+    case 'searchResult':
+      processSearchResults(e.data);
+      // var { message, concepts, possibleExtraWords, excludedConcepts } = e.data;
+      // console.log(
+      //   terminology,
+      //   message,
+      //   concepts,
+      //   possibleExtraWords,
+      //   excludedConcepts
+      // );
+      break;
+    case 'loading':
+      const { number, total } = e.data;
+      $wordInput.style.backgroundImage = `linear-gradient(to right, #00ffcc 0%, #00ffcc ${
+        (100 * number) / total
+      }%, #fff ${(100 * number) / total}%)`;
+      break;
+    case 'loaded':
+      worker[terminology].isDone = true;
+      const div = document.createElement('div');
+      div.innerText = `Loaded ${terminology} files.`;
+      $loader.appendChild(div);
+
+      if (worker.ctv3.isDone && worker.readv2.isDone && worker.snomed.isDone) {
+        $loader.style.display = 'none';
+        $wrapper.style.display = 'grid';
+        $wordInput.style.backgroundImage = '';
+        $wordInput.removeAttribute('disabled');
+        $wordInput.setAttribute('placeholder', 'Enter a search term...');
+        $wordInput.focus();
+      }
+
+      break;
+  }
+}
+
+worker.ctv3.onmessage = onWorkerMessage;
+worker.readv2.onmessage = onWorkerMessage;
+worker.snomed.onmessage = onWorkerMessage;
+
+let data;
+let latestId;
+let selectedTerminology = 'snomed';
+
+function elHeight(el) {
+  const styles = window.getComputedStyle(el);
+  return (
+    el.offsetHeight +
+    parseFloat(styles['margin-top']) +
+    parseFloat(styles['margin-bottom'])
+  );
+}
+const includedExtraWords = {};
+const potentialWords = {};
+$extraTerms.addEventListener('click', async (e) => {
+  if (e.target.dataset && e.target.dataset.word) {
+    //Is it the more info button ?
+    if (e.target.classList.contains('info-button')) {
+      const resp = await fetch(
+        `https://rxnav.nlm.nih.gov/REST/approximateTerm.json?term=${e.target.dataset.word}`
+      ).then((x) => x.json());
+      if (resp && resp.approximateGroup && resp.approximateGroup.candidate) {
+        const possibleMatches = resp.approximateGroup.candidate
+          .map((x) => x.name || false)
+          .filter(
+            (x) =>
+              x &&
+              x.toLowerCase().indexOf($wordInput.value.trim().toLowerCase()) >
+                -1
+          );
+        if (possibleMatches.length > 0) {
+          const el = document.createElement('div');
+          el.innerText = `RXNorm contains: ${possibleMatches[0]}`;
+          e.target.parentElement.parentElement.after(el);
+        } else {
+          window.open(
+            `https://www.google.com/search?q=${e.target.dataset.word}`,
+            '_blank'
+          );
+        }
+      } else {
+        window.open(
+          `https://www.google.com/search?q=${e.target.dataset.word}`,
+          '_blank'
+        );
+      }
+    } else {
+      if (e.target.classList.contains('included')) {
+        // removing
+        delete includedExtraWords[e.target.dataset.word];
+        e.target.classList.remove('included');
+        e.target.innerText = 'Include';
+      } else {
+        includedExtraWords[e.target.dataset.word] = true;
+        e.target.classList.add('included');
+        e.target.innerText = 'Remove';
+      }
+      search();
+    }
+  }
+});
+
+function displayExtraWords(words) {
+  $extraTerms.innerHTML = Object.keys(includedExtraWords)
+    .concat(words.filter((x) => !includedExtraWords[x]))
+    .map(
+      (word) =>
+        `<div class="potential-word">${word} <div><button data-word="${word}" class="info-button">More info</button><button data-word="${word}" class="${
+          includedExtraWords[word] ? 'included' : ''
+        }">${
+          includedExtraWords[word] ? 'Exclude' : 'Include'
+        }</button></div></div>`
+    )
+    .join('');
+}
+
+function processSearchResults({
+  terminology,
+  concepts,
+  possibleExtraWords,
+  message,
+  excludedConcepts,
+  id,
+}) {
+  if (id !== latestId) return;
+  data[terminology] = {
+    concepts,
+    message,
+    excludedConcepts,
+  };
+  if (possibleExtraWords) {
+    possibleExtraWords.forEach((word) => {
+      if (data.possibleExtraWords.indexOf(word) < 0) {
+        data.possibleExtraWords.push(word);
+      }
+    });
+  }
+  if (terminology === selectedTerminology) {
+    displayResults();
+  }
+}
+
+function displayResults() {
+  if (data[selectedTerminology].concepts) {
+    displayExtraWords(data.possibleExtraWords);
+    const originalLength = Object.entries(
+      data[selectedTerminology].concepts
+    ).length;
+
+    const items = Object.entries(data[selectedTerminology].concepts);
+    $message.innerText = data[selectedTerminology].message;
+    $descendantCodes.innerHTML = items
+      .map(([code, definition]) => {
+        return `<tr><td>${code}</td><td>${definition}</td></tr>`;
+      })
+      .join('');
+    document.querySelector('.scrollable-defs').style.height = `${
+      window.innerHeight - elHeight($termTab) - 36 // 36 = header
+    }px`;
+    data[selectedTerminology].data = items
+      .map(([code, definition]) => {
+        return `${code}\t${definition}`;
+      })
+      .join('\n');
+
+    // Excluded codes
+    const excludedItems = Object.entries(
+      data[selectedTerminology].excludedConcepts
+    );
+    if (excludedItems.length > 0) {
+      $excludedCodes.innerHTML = excludedItems
+        .map(([code, definition]) => `<div>${code}: ${definition}</div>`)
+        .join('');
+    } else {
+      $excludedCodes.innerHTML = '';
+    }
+
+    $copyAll.removeAttribute('disabled');
+  } else {
+    $copyAll.setAttribute('disabled', '');
+    $message.innerText =
+      (data && data.snomed && data.snomed.message) || `No matches`;
+    displayExtraWords([]);
+    $descendantCodes.innerHTML = '';
+  }
+}
+
+function search(mainWord = $wordInput.value.trim().toLowerCase()) {
+  latestId = Math.random();
+  data = { ctv3: {}, snomed: {}, readv2: {}, possibleExtraWords: [] };
+  const words = [mainWord].concat(Object.keys(includedExtraWords));
+  send('snomed', 'search', { words, id: latestId });
+  send('ctv3', 'search', { words, id: latestId });
+  send('readv2', 'search', { words, id: latestId });
+}
+
+$wordInput.addEventListener('input', () => {
+  const word = $wordInput.value.trim().toLowerCase();
+  if (word.length > 1) {
+    search(word);
+  } else {
+    $searchResult.innerHTML = '';
+  }
+});
+
+document.body.addEventListener('click', () => {
+  $searchResult.innerHTML = '';
+});
+
+$copyAll.addEventListener('click', async () => {
+  const now = new Date();
+  $copyAll.setAttribute('disabled', '');
+  $copyAll.innerText = 'Copying...';
+  // Copy the text inside the text field
+  await navigator.clipboard.writeText(data[selectedTerminology].data);
+
+  const diff = new Date() - now;
+  // var newWin = window.open('https://code-set-comparison.rw251.com/');
+  setTimeout(() => {
+    $copyAll.removeAttribute('disabled', '');
+    $copyAll.innerText = 'Copied!';
+    // newWin.body.style.backgroundColor = 'blue';
+    setTimeout(() => {
+      $copyAll.innerText = 'Copy all';
+    }, 2000);
+  }, Math.max(0, 500 - diff));
+});
+
+function showTerminology(terminology, element) {
+  document.querySelectorAll('#results .tablinks').forEach((el) => {
+    el.classList.remove('active');
+  });
+  selectedTerminology = terminology.toLowerCase();
+  if (data && data[selectedTerminology]) displayResults();
+  element.classList.add('active');
+}
---
+++ web/worker.template.js
@@ -0,0 +1,282 @@
+/*
+Get definitions
+*/
+const terminology = this.name;
+function log(msg) {
+  console.log(`W-${terminology.toUpperCase()}> ${msg}`);
+}
+
+onmessage = (e) => {
+  const { action, params } = e.data;
+  const { version, words, id } = params;
+  switch (action) {
+    case 'search':
+      search(words, id);
+      break;
+    default:
+      log(`Incorrect action received by worker: ${action}`);
+  }
+};
+
+function getChildren(code) {
+  if (terminology === 'readv2') code = code.substring(0, 5);
+  if (!o.rels[code]) return [];
+  return Object.keys(o.rels[code]);
+}
+
+function getDescendantList(conceptId) {
+  if (!o.defs[conceptId]) {
+    return [];
+  }
+  let ans = [];
+  let queue = [{ code: conceptId, level: 0 }];
+
+  while (queue.length > 0) {
+    const next = queue.pop();
+    ans.push(next);
+    const children = getChildren(next.code);
+    queue = queue.concat(
+      children.map((x) => {
+        return { code: x, level: next.level + 1 };
+      })
+    );
+  }
+  const list = ans.map((x) => {
+    x.def = o.defs[x.code];
+    return x;
+  });
+  return list;
+}
+const exclusionTerms = [
+  /\(disorder\)/i,
+  /\(procedure\)/i,
+  /\(substance\)/i,
+  /\(observable entity\)/i,
+  /\(qualifier value\)/i,
+  /\(situation\)/i,
+  /\(regime\/therapy\)/i,
+  / level$/i,
+  /\(finding\)/i,
+  /\(event\)/i,
+  /serum.*concentration/i,
+  /urine.*concentration/i,
+  /adverse.*reaction/i,
+];
+function shouldExclude(definition, code) {
+  const def =
+    typeof definition === 'string' ? definition : definition.join(' | ');
+  if (terminology === 'ctv3' && !o.medicalCodes[code]) return true;
+  if (terminology === 'readv2' && !code.match(/^[a-z]/)) return true;
+  return exclusionTerms.filter((x) => def.match(x)).length > 0;
+}
+function search(words, id) {
+  const concepts = {};
+  const excludedConcepts = {};
+  const possibleExtraWordsObject = {};
+  let message;
+  let originalLength = 0;
+  for (let i = 0; i < words.length; i++) {
+    const word = words[i];
+    if (o.words[word]) {
+      // That word exists
+      const newConcepts = {};
+      // For each code matching the word...
+      for (let j = 0; j < o.words[word].length; j++) {
+        const conceptId = o.words[word][j];
+        // get the definition (is either a string, or an array of multiple defs if ctv/read)
+        const definition = o.defs[conceptId];
+        // Go for last in array if multiple as that is the location of the longest def;
+        const singleDefinition =
+          typeof definition === 'string'
+            ? definition
+            : definition[definition.length - 1];
+        // check if we should exclude it
+        if (shouldExclude(definition, conceptId)) {
+          excludedConcepts[conceptId] = singleDefinition;
+        } else {
+          newConcepts[conceptId] = singleDefinition;
+          concepts[conceptId] = singleDefinition;
+        }
+        getDescendantList(conceptId).forEach(({ code, def }) => {
+          const singleDef = typeof def === 'string' ? def : def[def.length - 1];
+          if (shouldExclude(def, code)) {
+            excludedConcepts[code] = singleDef;
+          } else {
+            newConcepts[code] = singleDef;
+            concepts[code] = singleDef;
+          }
+        });
+        if (Object.keys(concepts).length > 10000) {
+          return postMessage({
+            msg: 'searchResult',
+            message: 'Too many matches',
+            terminology,
+            id,
+          });
+        }
+      }
+
+      if (i === 0) {
+        originalLength = Object.entries(concepts).length;
+        message = `- ${originalLength} concepts matched from "${word}"`;
+      }
+
+      // unmatched words
+      Object.values(newConcepts)
+        .filter((conceptDefinition) => {
+          // filter to those not already matching a search word
+          const searchWordsAppearingInDefinition = words.filter((eachWord) => {
+            return conceptDefinition.toLowerCase().indexOf(eachWord) > -1;
+          });
+          return searchWordsAppearingInDefinition.length === 0;
+        })
+        .map(
+          (conceptDefinition) => conceptDefinition.toLowerCase().split(' ')[0]
+        )
+        .filter(
+          (possibleWord) => ['product', 'generic'].indexOf(possibleWord) < 0
+        )
+        .forEach((possibleWord) => {
+          possibleWord = possibleWord.replace(/^\*/g, '');
+          possibleExtraWordsObject[possibleWord] = true;
+        });
+    }
+  }
+  const newLength = Object.keys(concepts).length;
+  const formatter = new Intl.ListFormat('en', {
+    style: 'long',
+    type: 'conjunction',
+  });
+  if (newLength > originalLength) {
+    message += `\n- A further ${
+      newLength - originalLength
+    } codes found by also searching for ${formatter.format(words.slice(1))}`;
+  }
+  return postMessage({
+    msg: 'searchResult',
+    message,
+    terminology,
+    concepts,
+    possibleExtraWords: Object.keys(possibleExtraWordsObject),
+    excludedConcepts,
+    id,
+  });
+}
+
+const findWords = (stub) => {
+  let pointer = o.trie;
+  stub
+    .toLowerCase()
+    .split('')
+    .forEach((letter) => {
+      pointer = pointer[letter];
+    });
+  if (!pointer) return [];
+  const words = [];
+  if (typeof pointer === 'string') {
+    return [stub + pointer];
+  }
+  const queue = Object.keys(pointer)
+    .map((x) => {
+      if (x === '1' && pointer[x] === true) {
+        words.push(stub);
+        return false;
+      }
+      return { p: pointer[x], s: stub + x };
+    })
+    .filter(Boolean);
+  while (queue.length > 0) {
+    const { p, s } = queue.shift();
+    if (p && typeof p === 'object') {
+      // already there and already an object
+      Object.keys(p).forEach((x) => {
+        if (x === '1' && p[x] === true) words.push(s);
+        else queue.push({ p: p[x], s: s + x });
+      });
+    } else if (p === true) {
+      // already there but a boolean
+      words.push(s);
+    } else if (p) {
+      // already there but a string
+      words.push(s + p);
+    } else {
+      //shoultn'd get here
+    }
+  }
+  return words;
+};
+
+const o = { medicalCodes: {} };
+function findDrugCodes(code) {
+  let queue = Object.keys(o.rels[code]);
+  while (queue.length > 0) {
+    const nextCode = queue.pop();
+    if (o.rels[nextCode]) {
+      Object.keys(o.rels[nextCode]).forEach((code) => {
+        queue.push(code);
+      });
+    }
+    o.medicalCodes[nextCode] = true;
+  }
+}
+
+let retrieved = 0;
+async function loadObject(name, r2Path, filename) {
+  console.time(name);
+  log(`Starting to fetch ${name}`);
+  o[name] = await fetch(
+    `{URL}/${r2Path.replace(/\/\//g, '/')}/${filename}`
+  ).then((x) => x.json());
+
+  if (name === 'rels' && terminology === 'ctv3') {
+    findDrugCodes('x00xm');
+    findDrugCodes('x025Q');
+  }
+  retrieved++;
+  postMessage({
+    msg: 'loading',
+    terminology,
+    number: retrieved,
+    total: objectsToLoad.length,
+  });
+  console.timeEnd(name);
+  if (retrieved === objectsToLoad.length) {
+    postMessage({ msg: 'loaded', terminology });
+  }
+}
+
+const filenames = {
+  ctv3: {
+    defs: 'defs-ctv3.json',
+    rels: 'relations-ctv3.json',
+    trie: 'trie.json',
+    words: 'words.json',
+  },
+  readv2: {
+    defs: 'defs-readv2.json',
+    rels: 'relations-readv2.json',
+    trie: 'trie.json',
+    words: 'words.json',
+  },
+  snomed: {
+    defs: 'defs-single.json',
+    rels: 'relationships.json',
+    trie: 'trie.json',
+    words: 'words.json',
+  },
+};
+
+const objectsToLoad = Object.entries(filenames.snomed).map(([name]) => {
+  return { name, filename: filenames[terminology][name] };
+});
+
+async function loadKeys() {
+  const keys = await fetch(`keys.json?v=${Math.random()}`).then((x) =>
+    x.json()
+  );
+  objectsToLoad.forEach(({ name, filename }) => {
+    loadObject(name, keys[terminology].r2Path, filename);
+  });
+}
+
+loadKeys();
